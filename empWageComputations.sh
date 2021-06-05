98a50a76c21a1b41c08ded39df31927d6b9d61b1
ashTrader17 <saaksshi17@gmail.com>
2021-06-05 08:36:40 +0530

    UC8 upated

--- empWageComputations.sh
+++ empWageComputations.sh
@@ -1,21 +1,21 @@
-
 #!/bin/bash
+declare -a dailyWage
 wage=0
 for((i=1;i<21;i++))
 do
-attend=$((RANDOM%3))
-if [ $attend -eq 1 ]
-then
-   time=$((RANDOM%801))
+   attend=$((RANDOM%3))
+   if [ $attend -eq 1 ]
+   then
+      time=$((RANDOM%8))
+      wage=$((time*20))
+   elif [ $attend -eq 2 ]
+   then
+      time=$((RANDOM%801))
       wage=$((wage+16*20))
-fi
-
-if [ $attend -eq 2 ]
-then
-   time=$((RANDOM%801))
-      wage=$((wage+16*20))
-fi
-
+   fi
+dailyWage[$i]=$wage
+total=$((wage+total))
 done
 
-echo "WAGE: "$wage
+echo "TOTAL WAGE: "$total
+echo "DAILY WAGE :"${dailyWage[*]}
