5ab7a3098a10c64c5f704f9ebdaeecb52d1d8e78
ashTrader17 <saaksshi17@gmail.com>
2021-06-05 08:25:52 +0530

    UC 6 updated

--- empWageComputations.sh
+++ empWageComputations.sh
@@ -3,17 +3,19 @@
 wage=0
 for((i=1;i<21;i++))
 do
-attend=$((RANDOM%2))
+attend=$((RANDOM%3))
 if [ $attend -eq 1 ]
 then
-   time=$((RANDOM%2))
-   if [ $time -eq 1 ]
-   then
-      wage=$((wage+8*20))
-   else
+   time=$((RANDOM%801))
       wage=$((wage+16*20))
-   fi
 fi
+
+if [ $attend -eq 2 ]
+then
+   time=$((RANDOM%801))
+      wage=$((wage+16*20))
+fi
+
 done
 
 echo "WAGE: "$wage
